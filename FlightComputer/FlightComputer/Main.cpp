9f919d55055fb00c78c689cb367b45eda82eb26c
McMac_McHat <edwards.cole97@gmail.com>
2018-10-01 20:06:43 -0700

    Check it out guys

---
+++ FlightComputer/FlightComputer/Main.cpp
@@ -0,0 +1,22 @@
+// 
+//  _      ___   _          _    __   __  ___    ___    _  _   ___    ___   ___ 
+// | |    | _ \ | |        /_\   \ \ / / |_ _|  / _ \  | \| | |_ _|  / __| / __|
+// | |__  |   / | |__     / _ \   \ V /   | |  | (_) | | .` |  | |  | (__  \__ \
+// |____| |_|_\ |____|   /_/ \_\   \_/   |___|  \___/  |_|\_| |___|  \___| |___/
+//                                                                            
+// Author:
+// Date Created: 
+// Date Revised:
+// Description:
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	// a test program with formatting
+	cout << "This is the first code for this project";
+
+}

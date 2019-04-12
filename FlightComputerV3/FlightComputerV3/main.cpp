6e25e65077699c114e9c335944b625882c7f51f5
McMac_McHat <edwards.cole97@gmail.com>
2019-04-12 16:55:37 -0700

    hey

--- FlightComputerV3/FlightComputerV3/main.cpp
+++ FlightComputerV3/FlightComputerV3/main.cpp
@@ -4,7 +4,7 @@
 #include <thread>
 #include <chrono>
 #include <wiringPi.h>
-
+// can you see this hoang?
 #include "state_machine.h"
 #include "input.h"
 #include "logger.h"
@@ -21,14 +21,14 @@ int main(void) {
 	initializePins();
 
 	// Create input threads
-	std::thread tPressureTransducerData(getPressureTransducerReadings);
+	//std::thread tPressureTransducerData(getPressureTransducerReadings);
 	std::thread tUserInput(getUserInput);
 
 	// Start the state machine
 	sm.run();
 
 	// Join input threads
-	tPressureTransducerData.join();
+	//tPressureTransducerData.join();
 	tUserInput.join();
 
 	// Wait to exit
